93dab24390089ae5b2420dfa4573715be6f3033e
Davi Torralvo <torralvodavi@gmail.com>
2025-08-15 14:59:51 -0300

    correção de bug de novo novo

--- webdev/settings.py
+++ webdev/settings.py
@@ -15,18 +15,25 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=38mnm=fn6qgsgf&&0i^&%7m%v4(zi5r7c=e3ecbyo^q1krlk'
+SECRET_KEY = 'django-insecure-5ah3trzu*lql^&ztorah@97kv0bd5^4*c-*41w6*rdrn$95u8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['davi123456789.pythonanywhere.com']
-
+# ALLOWED_HOSTS = ['pedroodake.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mycontacts'
 ]
 
 MIDDLEWARE = [
@@ -114,10 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
